047f25d900f2575aea58df14cc0cae29487397d5
Sandro Escate <1362503@senati.pe>
2023-05-12 12:31:35 -0500

    TemplateString

---
+++ templateString.js
@@ -0,0 +1,8 @@
+const nombre = 'Carlos Hernandez';
+const edad = 30;
+const pais = 'México';
+
+// Esto es templateString, nos permite trabajar con código JS en cadenas de texto.
+console.log(`La persona es ${nombre} tiene ${edad} y es de ${pais}`);
+
+
