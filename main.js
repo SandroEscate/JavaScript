84b38f4f949c8ac4a18bc56e0264abe4a7194b54
Sandro Escate <1362503@senati.pe>
2023-05-12 00:19:23 -0500

    Variables

--- main.js
+++ main.js
@@ -8,6 +8,22 @@ Tipos de datos:
     null      - Valor Nulo
     undefined - Valor no definido
 */
+// let se puede cambiar el valor, caso contrario con const no se puede.
 let nombre = "Sandro"; // string
 let valor = 27; // number
 let verdad = true; // boolean
+const correo = 'ejemplo@gmail.com';
+
+// Ahorrar lineas de código al declarar variables
+let telefono, pais, id;
+telefono = 123456789;
+
+// Operaciones con variables
+let valorUno = 20;
+let valorDos = 19;
+console.log(valorUno + valorDos)
+
+// JS es dinámico
+let miVariable = 'texto';
+miVariable = 7;
+
