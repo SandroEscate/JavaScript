fcd37637c8f8cff4c1d982fd7e1541bc52d543c6
Sandro Escate <1362503@senati.pe>
2023-05-12 00:11:53 -0500

    Tipos de datos

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert('Hola mundo, desde archivo externo');
+/*
+Tipos de datos:
+    string    - Cadena de texto
+    number    - Numero
+    boolean   - Booleano
+    object    - Objeto
+    function  - Funciones
+    null      - Valor Nulo
+    undefined - Valor no definido
+*/
+let nombre = "Sandro"; // string
+let valor = 27; // number
+let verdad = true; // boolean
